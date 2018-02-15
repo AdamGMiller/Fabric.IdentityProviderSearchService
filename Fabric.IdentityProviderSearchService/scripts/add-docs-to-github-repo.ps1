f352ae1646ea86e07b993128cca57fe1dc9f8a51
Kyle Paul <djjoebanks@gmail.com>
2018-02-15 14:44:38 -0800

    fix a bug in the script

--- Fabric.IdentityProviderSearchService/scripts/add-docs-to-github-repo.ps1
+++ Fabric.IdentityProviderSearchService/scripts/add-docs-to-github-repo.ps1
@@ -9,7 +9,7 @@ function ReplaceStrings ($stringToFind, $stringToReplace) {
 }
 
 Write-Host "changing to markdown directory"
-Set-Location $2
+Set-Location "$2"
 
 Write-Host "Cloning Git Wiki repo..."
 $REPO="https://$1@github.com/HealthCatalyst/Fabric.IdentityProviderSearchService.wiki.git"
