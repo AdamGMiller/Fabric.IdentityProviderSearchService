eca493612840218ce5e8ddc6a796eab7ce681d5f
michael.vidal <michael.vidal@healthcatalyst.com>
2018-03-23 10:55:03 -0400

    Added script to register with Discovery on install. Added script to run functional tests locally.

---
+++ Fabric.IdentityProviderSearchService/scripts/run-functional-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+docker stop idpsearch-functional-identity
+docker rm idpsearch-functional-identity
+docker network rm idpsearch-functional-tests
+
+docker pull healthcatalyst/fabric.identity
+
+docker network create idpsearch-functional-tests
+
+docker run -d --name idpsearch-functional-identity \
+	-p 5001:5001 \
+	-e "HostingOptions__StorageProvider=InMemory" \
+	-e "HostingOptions__AllowUnsafeEval=true" \
+	-e "IssuerUri=http://localhost:5001" \
+	-e "IDENTITYSERVERCONFIDENTIALCLIENTSETTINGS__AUTHORITY=http://localhost:5001" \
+	--network="idpsearch-functional-tests" \
+	healthcatalyst/fabric.identity
+echo "started identity"
+sleep 3
+
+output=$(curl -sSL https://raw.githubusercontent.com/HealthCatalyst/Fabric.Identity/master/Fabric.Identity.API/scripts/setup-samples.sh | sh /dev/stdin http://localhost:5001)
+echo $output
+installerSecret=$(echo $output | grep -oP '(?<="installerSecret":")[^"]*')
+export FABRIC_INSTALLER_SECRET=$installerSecret
+echo $installerSecret
+export BASE_IDP_SEARCH_URL=http://localhost:55655
+
+read -p "Start the idpsearch service, then press enter to continue."
+
+cd ../../Fabric.IdentityProviderSearch.FunctionalTests
+
+npm install
+npm test
+
+docker stop idpsearch-functional-identity
+docker rm idpsearch-functional-identity
+docker network rm idpsearch-functional-tests
